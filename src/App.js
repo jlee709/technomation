31f839f442b37bb2ed33d0db3851664db04f25fd
Justin Lee <leejwg.3@gmail.com>
2018-01-23 22:29:08 -1000

    bug fixing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { Header } from './components/common';
-
+import LibList from './components/LibList';
 
 const App = () => {
   return (
     <Provider store={createStore}>
       <View>
         <Header headerText="Technomation" />
+        <LibList />
       </View>
     </Provider>
   );
