4acc764430575448eb08f842ca69d60da7db3f81
Justin Lee <leejwg.3@gmail.com>
2018-01-23 15:43:29 -1000

    small big fix

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { Header } from './components/common';
+
 
 const App = () => {
   return (
     <Provider store={createStore}>
-     <View />;
+      <View>
+        <Header headerText="Technomation" />
+      </View>
     </Provider>
   );
 };
