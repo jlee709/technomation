e01a13f11896fd52e04f7c56e59223bab12d7f13
Justin Lee <leejwg.3@gmail.com>
2018-01-23 15:38:48 -1000

    added main reusable components to seperate file

---
+++ src/components/common/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// header cmp
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+  return(
+    <View style={viewStyle}>
+    <Text style={textStyle}> {props.headerText}</Text>
+    </View>
+    );
+};
+
+const styles = {
+  viewStyle:{
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: {width:0, height:5},
+    shadowOpacity: 0.25,
+    elevation: 2,
+    position: 'relative'
+
+  },
+  textStyle: {
+    fontSize: 25
+  }
+};
+
+// exporting for use ~
+export { Header };
