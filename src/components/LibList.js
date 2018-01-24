7d543e07af18c670138d12a03966dd91415c2ed9
Justin Lee <leejwg.3@gmail.com>
2018-01-23 21:48:18 -1000

    fixed tiny bug

--- src/components/LibList.js
+++ src/components/LibList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class LibList extends Component {
   render() {
@@ -6,4 +7,4 @@ class LibList extends Component {
   }
 }
 
-export default Liblist;
+export default connect()(Liblist);
