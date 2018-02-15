662c2b193344d4029b157c1a911b051c89feea27
Justin Lee <leejwg.3@gmail.com>
2018-02-15 20:53:05 +0900

    making ListView

--- src/components/LibList.js
+++ src/components/LibList.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 
 class LibList extends Component {
+
+  componenetWillMount(){
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1,r2) => r1 !== r2
+    });
+    //creates the Data to be rendered in the list, great for large renders
+    // Check official Docs to see example on this ListView
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow(){
+
+  }
+
   render() {
-    return;
+    console.log(this.props);
+    return(
+      <ListView 
+      dataSource={this.dataSource}
+      renderRow={this.renderRow}
+      />
+      );
   }
 }
 
 const mapStateToProps = state => {
-  console.log(state);
+  return {libraries: state.libraries};
 };
 
 export default connect(mapStateToProps)(LibList);
